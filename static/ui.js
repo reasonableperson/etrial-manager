72a3d7c028f4d22cb4dfe4ba695e15605d9b5185
Scott Young <scott@sjy.id.au>
2020-01-28 09:58:03 +1100

    rewrite js to be more ie11 friendly

--- static/ui.js
+++ static/ui.js
@@ -4,33 +4,45 @@ document.addEventListener('DOMContentLoaded', function () {
   const flashes = document.getElementById('flashes')
   if (flashes) {
     flashes.style.opacity = 1
-    setTimeout(() => { flashes.style.opacity = 0 }, 3000)
+    setTimeout(function () { flashes.style.opacity = 0 }, 3000)
   }
 
-  // #drag-drop-overlay is an absolutely-positioned div with partial opacity
-  // which covers the entire viewport and is not displayed by default. These
-  // functions can be used to 'highlight' the viewport, to indicate that it is
-  // ready to receive a dropped file, by making #highlight visible.
+  // Disable the browser's built-in functionality. When a user drags a PDF onto
+  // the window, it should be uploaded, not displayed.
+  // Reference: https://stackoverflow.com/a/6756680/1402935
+  const ignoreEvent = function (e) { e = e || event; e.preventDefault() }
+  window.addEventListener('dragover', ignoreEvent)
+  window.addEventListener('drop', ignoreEvent)
+
+  // Highlight the documents table when the user drags a file onto the window,
+  // by adding a class to the invisible overlay element.
   const overlay = document.getElementById('drag-drop-overlay')
-  const highlight = function (event)  {
-    event.preventDefault()
+  var dragCount = 0
+  window.addEventListener('dragenter', function () {
+    // The cursor may drag into an element, then into one of its child elements.
+    // Keep track of the 'depth' of the drag event.
+    dragCount = dragCount + 1
     overlay.classList.add('active')
-  } 
-  const unhighlight = function (event) {
-    if (event) event.preventDefault()
-    overlay.classList.remove('active')
-  }
+  })
+  window.addEventListener('dragleave', function () {
+    dragCount = dragCount - 1
+    // Only when the cursor has left the screen altogether ('clearing' all
+    // previous dragenter events) should we disable the overlay.
+    if (dragCount == 0) overlay.classList.remove('active')
+  })
 
-  // Given a drop event, call upload() once for each file
-  const handleDroppedFiles = function (event) {
+  // Given a drop event, call upload() once for each file. TODO: multiple file
+  // upload doesn't work in Firefox.
+  window.addEventListener('drop', function (event) {
     event.preventDefault()
-    unhighlight()
+    overlay.classList.remove('active')
     const files = event.dataTransfer.files
+    console.log(files.length + " files", files)
     for (var i = 0; i < files.length; i++) {
       upload(files[i])
       console.dir(files[i].name, files[i].size / 1024, files[i].type)
     }
-  }
+  })
 
   // Create a new row in the documents table to display the upload progress
   // indicator. Returns a div which should have its width adjusted to reflect
@@ -55,7 +67,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Given a file (and a single drag and drop event could yield multiple files),
   // upload it to the backend. 
+  var uploadCount = 0
   const upload = function (file) {
+    uploadCount = uploadCount + 1
     const url = '/documents/add?filename=' + encodeURIComponent(file.name)
     // Create an old-fashioned XMLHttpRequest, because the Fetch API doesn't
     // provide great support for sending back upload progress reports at this
@@ -63,16 +77,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // not the XHR itself.
     var req = new XMLHttpRequest()
     const progressIndicator = createProgressIndicator(file)
-    req.upload.addEventListener('progress', (e) => {
+    req.upload.addEventListener('progress', function (e) {
       progressIndicator.style.width = e.loaded / e.total * 100 + '%'
     })
 
-    // Attach an event handler on the request itself to refresh the page when
-    // the upload is done, provided a favourable response was received from the
-    // backend.
-    req.addEventListener('load', () => {
-      if (req.status == 200) window.location = '?reverse'
-      else console.error(req.response)
+    // When an upload finishes, decrement the count of remaining uploads, and
+    // if it was the last upload, refresh the page.
+    req.addEventListener('load', function () {
+      if (req.status == 200) {
+        uploadCount = uploadCount - 1
+        if (uploadCount == 0) window.location.reload()
+      }
+      else { console.error(req.response) }
     })
     req.open('POST', url, true)
     req.send(file)
@@ -80,27 +96,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Given a click event from the documents or users table, generate the correct
   // backend request by inspecting the clicked cell's data attributes.
-  const handleMatrix = async function (e) {
+  const handleMatrix = function (e) {
     // don't do anything if the handler is fired by something other than a <td>
     if (e.target.tagName != "TD" || !e.target.classList.contains("matrix")) return
     const d = e.target.dataset
     const url = [window.location.pathname, d.action, d.row, d.col].join('/')
     console.log(url)
-    const response = await fetch(url, { 'method': 'POST' })
-    if (response.status == 200) window.location.reload()
-    else console.error(response)
+    var req = new XMLHttpRequest()
+    req.addEventListener('load', function () {
+      if (req.status == 200) window.location.reload()
+      else console.error(req.response)
+    })
+    req.open('POST', url, true)
+    req.send()
   }
 
   // Attach click handlers to the specified elements, if they exist.
-  const maybeAttach = (selector, _event, callback) => {
+  const maybeAttach = function (selector, _event, callback) {
     const el = document.querySelector(selector)
     if (el != null) el.addEventListener(_event, callback, false)
   }
   maybeAttach('#documents', 'click', handleMatrix)
   maybeAttach('#users', 'click', handleMatrix)
-  maybeAttach('.drag-drop-target', 'dragenter', highlight)
-  maybeAttach('.drag-drop-target', 'dragover', highlight)
-  maybeAttach('.drag-drop-target', 'dragleave', unhighlight)
-  maybeAttach('.drag-drop-target', 'drop', handleDroppedFiles)
 
 })
