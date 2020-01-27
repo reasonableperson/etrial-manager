a9cafd1a9ee8eab306c6bcb5f61e15f14bfd7e1f
Scott Young <scott@sjy.id.au>
2020-01-27 19:42:08 +1100

    add some more checks to create-gocryptfs

--- scripts/create-gocryptfs.sh
+++ scripts/create-gocryptfs.sh
@@ -2,20 +2,27 @@
 
 if [[ $(</proc/sys/kernel/hostname) != "etrial" ]]; then
   echo "You should run this from inside the container."
-  exit
+  exit 1
 fi
 
 if [[ $(whoami) != "etrial" ]]; then
   echo "You should run this as the etrial user so that the resulting data is owned by the correct user."
-  exit
+  exit 1
 fi
 
 if [[ "$1" == "purge" ]]; then
+  # echo "Purging ..."
   fusermount -u /crypt
   rm -rf ~etrial/crypt
 fi
 
-key=$(pwgen -1 4 4)
-echo $key
-mkdir -p ~etrial/crypt
-gocryptfs -passfile <(echo $key) -init ~etrial/crypt 2>&1
+if [ -d ~etrial/crypt ]; then
+  # echo "~etrial/crypt already exists; refusing to proceed."
+  exit 1
+else
+  key=$(pwgen -1 4 4)
+  echo $key
+  mkdir -p ~etrial/crypt
+  gocryptfs -passfile <(echo $key) -init ~etrial/crypt 2>&1
+fi
+
